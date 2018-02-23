a26b7fa7490f24e8212111a50685543b86f076df
TomEilers <32219903+TomEilers@users.noreply.github.com>
2018-02-23 14:10:42 +0000

    Fairness Code

---
+++ draft/Fairness.cpp
@@ -0,0 +1,48 @@
+int main()
+{
+  string text;
+  int fscore = 0;
+  bool eins, zwei, drei;
+  
+  while (true){
+      cout << "Wird Wunsch 1 erfüllt? J/N" << "\n";
+      cin >> text;
+      if (text == "J")
+        eins = true;
+      else 
+        eins = false; 
+      
+      cout << "Wird Wunsch 2 erfüllt? J/N" << "\n";
+      cin >> text;
+      if (text == "J")
+        zwei = true;
+      else 
+        zwei = false; 
+      
+      cout << "Wird Wunsch 3 erfüllt? J/N" << "\n";
+      cin >> text;
+      if (text == "J")
+        drei = true;
+      else 
+        drei = false; 
+        
+      if (eins) 
+        fscore = fscore + 6;
+      else 
+        fscore --;
+        
+      if (zwei) 
+        fscore = fscore + 4;
+      else 
+        fscore --;
+        
+      if (drei) 
+        fscore = fscore + 2;
+      else 
+        fscore --;
+        
+      cout << "Der Fairnesscore ist " << fscore << "\n";
+      
+      fscore = 0;
+  }
+}
